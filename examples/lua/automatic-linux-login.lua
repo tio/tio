96fafc5fb4f9cac808910272dcade06b548fcd78
Martin Lund <martin.lund@keep-it-simple.com>
2024-04-19 14:44:54 +0200

    Generalize automatic login example for Linux

--- examples/lua/automatic-linux-login.lua
+++ examples/lua/automatic-linux-login.lua
@@ -13,17 +13,16 @@ local logins = {
     },
 }
 
-send("\n")
 local found, match_str = expect("\\w+- login:", 10)
 if (1 == found) then
-    local model = string.match(match_str, "^%w+")
-    local login = logins[model]
+    local hostname = string.match(match_str, "^%w+")
+    local login = logins[hostname]
     if (nil ~= login) then
         send(login.username .. "\n")
         expect("Password:")
         send(login.password .. "\n")
     else
-        print("\r\nDon't know login info for " .. model .. "\r\n")
+        print("\r\nDon't know login info for " .. hostname .. "\r\n")
     end
 else
     print("\r\nDidn't find a login prompt\r\n")
